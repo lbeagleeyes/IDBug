d845d3ca5d65ef905c71c79360b7c7986c89b4a7
kdublam <kdublam@gmail.com>
2019-04-13 13:01:55 -0700

    added logout option to the app and included a few requirements for user signup (password length, matching password). user can also hit enter to submit the form

--- assets/javascript/signin.js
+++ assets/javascript/signin.js
@@ -9,29 +9,74 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Call Sign up button function when the signup button is clicked
-$(document).on("click", "#signupbtn", createNewuser);
 
+// Call Sign up button function when the signup button is clicked 
+// or when the user hit the Enter key
+$(document).on("click", "#signupbtn", createNewuser);
+$(document).on("keyup", "#confirmpassword", function(event){
+  if (event.key == "Enter") {
+      $("#signupbtn").click();
+  }
+})
+$(document).on("keyup", "#signuppassword", function(event){
+  if (event.key == "Enter") {
+      $("#signupbtn").click();
+  }
+})
+
+$(document).on("keyup", "#signupemail", function(event){
+  if (event.key == "Enter") {
+      $("#signupbtn").click();
+  }
+})
 
 // Create new user function is called when sign up button is clicked
 function createNewuser() {
   event.preventDefault();
   var email = $("#signupemail").val().trim();
   var password = $("#signuppassword").val().trim();
+  var confirmpassword = $("#confirmpassword").val().trim();
+  var displayName = $("#name").val().trim();
 
   console.log(email);
   console.log(password);
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+  if (email === "") {
+    alert("Email cannot be blank");
+  }
+  if (password != confirmpassword) {
+    alert("Passwords don't match");
+  }
+  if (password === "" || confirmpassword === "") {
+    alert("Please enter a password")
+  }
+  if (password.length < 6 && confirmpassword.length < 6) {
+    alert("Password must have at least 6 characters")
+  }
+  if (password.length >= 6 && password === confirmpassword && email != "" && password != "" && confirmpassword != "") {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
+
 }
 
 // Call function to login user when login is clicked
+// or when the user hit the Enter key
 $(document).on("click", "#login", signinUser);
+$(document).on("keyup", "#loginpassword", function(event){
+  if (event.key == "Enter") {
+      $("#login").click();
+  }
+})
+$(document).on("keyup", "#loginemail", function(event){
+  if (event.key == "Enter") {
+      $("#login").click();
+  }
+})
 
 // Sign in user when the login button is clicked
 function signinUser() {
@@ -43,8 +88,9 @@ function signinUser() {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(error.message)
-    alert("Incorrect login")
+    $('#loginerror').modal('show');
+
+
     // ...
 
   });
@@ -55,11 +101,12 @@ firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
     window.location.replace("app.html");
-    console.log(user);
+    // console.log(user);
     // ...
   } else {
-    
+
     // User is signed out.
     // ...
   }
 });
+
