534cd0b4406e83c1c6702381d04497bcb7284298
Laura B <laurabermudezg@gmail.com>
2019-04-15 19:44:09 -0700

    adding google maps link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,17 @@
-//global variables
-var destination;
-var origin;
-var travelmode;
+function createGoogleMapsLink(destination) {
 
-//direction query URL that takes the user to Google Maps with origin and destination already plugged into the search. Also includes travelmode (ie. walking, driving, bicycle)
-// var directionQuery = "https://www.google.com/maps/dir/?api=1&origin=" + replaceSpace(origin) + "&destination=" + replaceSpace(destination) + "&travelmode=" + travelmode;
+    //direction query URL that takes the user to Google Maps with origin and destination already plugged into the search. Also includes travelmode (ie. walking, driving, bicycle)
+    var directionQuery = "https://www.google.com/maps/dir/?api=1&origin=" + usrLctn.longitude +','+ usrLctn.latitude+ "&destination=" + replaceSpace(destination) + "&travelmode=driving";
+
+    return directionQuery;
+    
+}
 
 //function to replace space with + for the url
-// function replaceSpace(location){
-//     return location.split(' ').join('+');
-// }
+function replaceSpace(loc) {
+    return loc.split(' ').join('+');
+}
+
 
 
 
