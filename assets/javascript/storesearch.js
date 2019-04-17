e55a8ea872aae2a45aa26a83bdbba200b3cc9f57
kdublam <kdublam@gmail.com>
2019-04-16 21:22:03 -0700

    fixed table with search button added

--- assets/javascript/storesearch.js
+++ assets/javascript/storesearch.js
@@ -96,35 +96,14 @@ firebase.auth().onAuthStateChanged(function (user) {
           //     }
           //   });
 
-          for(var i = 0; i < userSymptoms.length; i++){
+          for (var i = 0; i < userSymptoms.length; i++) {
             symCol.append(userSymptoms[i])
           }
 
           symptomsCol.append(symCol);
           row.append(symptomsCol);
 
-          // $("#searchList").append("<tr><td>"+userGender+"</td><td>"+userBirthYear+"</td><td>"+userSymptoms+"</td></tr>");
-
-          $("#searchList").append(row);  
-
-          //   $("#searchdisplay").append("<div class='modal fade' id='exampleModalLong' tabindex='-1' role='dialog' aria-labelledby='exampleModalLongTitle' aria-hidden='true'>" + "<div class='modal-dialog' role='document'>" +
-          //     "<div class='modal-content'>" +
-          //     "<div class='modal-header'>" +
-          //     "<h5 class='modal-title' id='exampleModalLongTitle'>" + "Modal title" + "</h5>" +
-          //     "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
-          //     "<span aria-hidden='true'>" + "&times;" + "</span>" +
-          //         "</button >" +
-          //       "</div >" +
-          //     "<div class='modal-body'>"
-          //       + modal body +
-          //       "</div>" +
-          //     "<div class='modal-footer'>" +
-          //       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          //       <button type="button" class="btn btn-primary">Save changes</button>
-          //     </div>
-          //     </div >
-          //   </div >
-          // </div > ")
+          $("#searchList").append("<tr><td>" + userGender + "</td><td>" + userBirthYear + "</td><td>" + userSymptoms + "</td>" + "<td><button type='button' class='btn btn-primary'>Search</button></td></tr>");
 
         }
 
